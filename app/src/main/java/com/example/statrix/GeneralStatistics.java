de2457b1ff0c1981db7bf3a2b10fb8f4f3b4467e
MikovIvan <mikov_ivan@list.ru>
2017-08-08 21:07:02 +0300

    create GeneralStatistics.java activity

---
+++ app/src/main/java/com/example/statrix/GeneralStatistics.java
@@ -0,0 +1,13 @@
+package com.example.statrix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeneralStatistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.general_statistics_filter);
+    }
+}
